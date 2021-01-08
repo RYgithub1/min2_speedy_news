6863a9b3597fa06094798548f6b197f2a3cf895b
RYgithub1 <ryo.rec.otani@gmail.com>
2021-01-08 09:12:20 +0900

    Make Consumer, PageTransformer, Opacity

--- lib/view/screens/pages/headline_page.dart
+++ lib/view/screens/pages/headline_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:min2_speedy_news/data/category_info.dart';
 import 'package:min2_speedy_news/data/search_type.dart';
+import 'package:min2_speedy_news/view/components/page_transformer.dart';
 import 'package:min2_speedy_news/viewmodels/headline_viewmodel.dart';
 import 'package:provider/provider.dart';
 
@@ -19,7 +20,40 @@ class HeadlinePage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-        body: Center(child: Text("headline")),
+        body: Consumer<HeadlineViewModel>(
+          builder: (context, headlineViewModel, child){
+            return PageTransformer(   /// [+PageTransformer]
+              pageViewBuilder: (context, pageVisivilityResolver) {
+                return PageView.builder(
+                  controller: PageController(initialPage: 1),   /// [PageView need controller]
+                  itemCount: headlineViewModel.articles.length,
+                  itemBuilder: (context, index) {
+                    final article = headlineViewModel.articles[index];
+                    final pageVisivility = pageVisivilityResolver.resolvePageVisibility(index);   /// [+PageTransformer]
+                    final visivilityFraction = pageVisivility.visibleFraction;   /// [+PageTransformer]
+
+                    return Opacity(   /// [+PageTransformer ... opacityコラボでページスクロールに合わせて透過]
+                      opacity: visivilityFraction,   /// [+PageTransformer]
+                      child: Container(
+                        child: Center(
+                          child: Column(
+                            children: <Widget>[
+                              Text(article.title),
+                              Text(article.description),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                );
+              },
+            );
+          },
+        ),
+
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.refresh),
           onPressed: () => onRefresh(context),   /// [pass Function: () => method,]
